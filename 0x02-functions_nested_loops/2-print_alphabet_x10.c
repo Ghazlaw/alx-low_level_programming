e36585578dc9155b1df4c6046a2736063d27a8e2
Ghazlaw <ghazalibraheem@gmail.com>
2022-12-13 03:18:51 -0800

    printing something multiple times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - program to print_alphabet_x10
+ * by Ghazlaw
+ */
+void print_alphabet_x10(void);
+{
+	char alphabets;
+	int count = 0;
+
+	while (count++ <= 9)
+	{
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabts++)
+		_putchar(alphabets);
+	_putchar("\n");
+	}
+}
